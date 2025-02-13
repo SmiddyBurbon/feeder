e08948925ffa62b690fcb4210dca395328815bed
Dennis Schmidt <me@dennisschmidt.net>
2025-02-13 20:21:45 +0100

    Added jeddah

--- src/assets/js/eformel.js
+++ src/assets/js/eformel.js
@@ -4,7 +4,8 @@ export function getCountry(city) {
   /*------VENUES------*/
 
   if (country.includes("diriyya")) { return "sa"; }
-  if (country.includes("diriyah")) { return "sa"; }
+  else if (country.includes("diriyah")) { return "sa"; }
+  else if {country.includes("jeddah")} { return "sa"; }
   else if (country.includes("marrakesch")) { return "ma"; }
   else if (country.includes("marrakesh")) { return "ma"; }
   else if (country.includes("marrakech")) { return "ma"; }
